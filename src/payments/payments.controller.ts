fbaf24ba837c52676ae465468bb87e1f816c9dc9
Nicolas Agosti <agostinicolas6557@gmail.com>
2024-09-12 11:14:50 -0300

    FirstCommit

---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
+
+@Controller('payments')
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('create-payment-session')
+  createPaymentSession(@Body() paymentSessionDto: PaymentSessionDto){
+
+    return this.paymentsService.createPaymentSession(paymentSessionDto);
+  }
+
+  @Get('success')
+  succes(){
+    return{
+      ok:true,
+      message:'Payment successful'
+    }
+  }
+  @Get('cancelled')
+  cancelled(){
+    return{
+      ok:false,
+      message:'Payment canceled'
+    }
+  }
+
+  @Post('webhook')
+  async stripeWebhook(@Req() req: Request,@Res() res: Response){
+    return this.paymentsService.stripeWebhook(req,res);
+  }
+
+
+}
